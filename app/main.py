b5965132b94ac388958075a5145d93c0979ce5b9
Ilyas Rashitov <rashtv.19@gmail.com>
2023-12-19 01:16:12 +0600

    add cors setting

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.endpoints import (
     users,
@@ -13,6 +14,19 @@ from app.api.endpoints import (
 from app.database import Base, engine
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",  # Update with your Vue.js development server URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(bank_accounts.router, prefix="/accounts", tags=["bank_accounts"])
